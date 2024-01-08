36cd97d09db093b33e7731707ece6e005a5c1d24
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2024-01-08 15:38:56 -0300

    Comentários ClassesHerois.js

--- Teste_ClassesHerois/ClassesHerois.js
+++ Teste_ClassesHerois/ClassesHerois.js
@@ -1,13 +1,16 @@
+// Definindo a classe Heroi com um construtor que recebe nome, idade e tipo como parâmetros
 class Heroi {
   constructor(nome, idade, tipo) {
-    this.nome = nome;
-    this.idade = idade;
-    this.tipo = tipo;
+    this.nome = nome;   // Atributo para armazenar o nome do herói
+    this.idade = idade; // Atributo para armazenar a idade do herói
+    this.tipo = tipo;   // Atributo para armazenar o tipo do herói (guerreiro, mago, monge, ninja)
   }
 
+  // Método atacar que exibe a mensagem de ataque conforme o tipo do herói
   atacar() {
     let ataque;
 
+    // Utilizando switch para determinar o tipo de ataque com base no tipo do herói
     switch (this.tipo) {
       case 'mago':
         ataque = 'usou magia';
@@ -25,6 +28,7 @@ class Heroi {
         ataque = 'usou um ataque indefinido';
     }
 
+    // Exibindo a mensagem do ataque no console
     console.log(`o ${this.tipo} atacou usando ${ataque}`);
   }
 }
